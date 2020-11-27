e1e96608a392ad8c2ed2ba47d7e8539ecc515add
otlichnny <12508523+otlichnyy@users.noreply.github.com>
2020-11-27 09:21:28 +0530

    add utility.css icomoon.css to _app.tsx (#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import App from 'next/app'
 import '../styles/main.css'
+import '../styles/utility.css'
+import '../styles/icomoon.css'
 
 class Application extends App {
   render(): JSX.Element {
